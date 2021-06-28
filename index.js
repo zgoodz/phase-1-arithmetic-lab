0aeb589618e5a788a42036df4018693cc27d3dfe
Zachary Goodman <znote89@gmail.com>
2021-06-28 16:33:05 -0400

    commit tree 2c1f437cc45187fd2d4c6969e8de7148c4de025f

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 14;
+const num4 = 5;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(19, 20);
